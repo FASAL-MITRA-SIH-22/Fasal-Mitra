809e06c99650d352bcaed438002c05db3e0f1148
Dhruvsolanki345 <dhruvsolanki753@gmail.com>
2022-03-19 18:01:45 +0530

    joi validation schema added

---
+++ api-gateway/validations/Auth.validation.js
@@ -0,0 +1,47 @@
+const Joi = require("joi");
+
+const loginSchema = Joi.object({
+  username: Joi.string()
+    .trim()
+    .pattern(/^[a-zA-Z0-9_@\.\-]+$/)
+    .required(),
+  password: Joi.string()
+    .trim()
+    .pattern(/^[a-zA-Z0-9!@#%^&*+-=]{6,15}$/)
+    .required(),
+});
+
+const registerSchema = Joi.object({
+  firstName: Joi.string()
+    .trim()
+    .min(3)
+    .max(20)
+    .pattern(/^[a-zA-Z_]*$/)
+    .required(),
+  lastName: Joi.string()
+    .trim()
+    .min(3)
+    .max(20)
+    .pattern(/^[a-zA-Z_]*$/)
+    .required(),
+  email: Joi.string().trim().email().lowercase(),
+  phone: Joi.string()
+    .trim()
+    .pattern(/^\d{10}$/)
+    .required(),
+  username: Joi.string()
+    .trim()
+    .pattern(/^[a-zA-Z0-9_@\.\-]+$/)
+    .required(),
+  password: Joi.string()
+    .trim()
+    .pattern(/^[a-zA-Z0-9!@#%^&*+-=]{6,15}$/)
+    .required(),
+  avatar: Joi.string().trim().uri(),
+  type: Joi.string().trim().valid("farmer", "expert").required(),
+});
+
+module.exports = {
+  loginSchema,
+  registerSchema,
+};

bb053810e68372c4855de4604267d4e47fda7d4d
Dhruvsolanki345 <dhruvsolanki753@gmail.com>
2022-03-23 23:40:19 +0530

    FIXES: environmental vairables moved to docker

--- dl/app/__init__.py
+++ dl/app/__init__.py
@@ -23,7 +23,8 @@ def create_app():
     app = Flask(__name__)
     app.register_blueprint(blueprint)
     app.json_encoder = JSONEncoder
-    app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+    # app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+    app.config["MONGO_URI"] = "mongodb+srv://fasalmitra:fasalmitra2022@cluster0.k8bxu.mongodb.net/mainDB?retryWrites=true&w=majority"
     from app.db_config import mongo
     mongo.init_app(app)
     return app
